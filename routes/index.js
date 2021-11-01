c8b9da78ffe7837911224049b2e64cd591a98054
Jake Swackhammer <j.100101@gmail.com>
2021-11-01 15:21:27 -0400

    fixes #42

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api/index.js');
+const homeRoutes = require('./homeRoutes.js');
 
-
+router.use('/', homeRoutes)
 router.use('/api', apiRoutes);
 
 module.exports = router;
