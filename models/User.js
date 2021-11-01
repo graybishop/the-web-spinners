17c554a5e6571afc3bda0f11607b97fa6e16e134
Mohamed Lazrek <mohamed.lazrekb@gmail.com>
2021-11-01 12:21:06 -0400

    fixes #16 #18

---
+++ models/User.js
@@ -0,0 +1,51 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
+
+// create our User model
+class User extends Model {
+  async checkPassword(loginPw) {
+    return await bcrypt.compare(loginPw, this.password);
+  }
+}
+
+User.init(
+  {
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8],
+      },
+    },
+  },
+  {
+    hooks: {
+      beforeCreate: async (newUserData) => {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+      beforeUpdate: async (updatedUserData) => {
+        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        return updatedUserData;
+      },
+    },
+    sequelize,
+    timestamps: false,
+    modelName: 'user'
+  }
+);
+
+module.exports = User;
