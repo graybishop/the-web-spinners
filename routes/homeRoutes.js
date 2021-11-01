2bd57da87bb953b12728f64dd0aa0c4e1e09505f
Jake Swackhammer <j.100101@gmail.com>
2021-11-01 17:32:36 -0400

    fixes $47

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -2,10 +2,21 @@ const router = require('express').Router();
 // eslint-disable-next-line no-unused-vars
 const { Venue } = require('../models');
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  let firstSetData = await Venue.findAll({limit:6})
+  
+  const maxTextSize = 150 //higher numbers mean more letters on the homepage cards descriptions
+  let firstSet = firstSetData.map(element => {
+    let data = element.toJSON()
+    if(data.description.length > maxTextSize){
+      data.description = data.description.substr(0, maxTextSize) + '...'
+    }
+    return data
+  })
+
   res.render('home', {
     title: 'Unearthly Venues',
-    venues: [1,2,3,4,5]
+    venues: firstSet
   })
 })
 
