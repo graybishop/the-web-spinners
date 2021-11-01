4058d031ec441b89fe50a9a888a4d1cdb430cd80
Jake Swackhammer <j.100101@gmail.com>
2021-11-01 14:35:53 -0400

    fixes #38

--- models/index.js
+++ models/index.js
@@ -2,6 +2,10 @@ const Venue = require('./Venue.js')
 const Review = require('./Review.js');
 const User = require('./User.js');
 
+User.hasMany(Review)
+Review.belongsTo(User)
 
+Venue.hasMany(Review)
+Review.belongsTo(Venue)
 
 module.exports = {Venue, User, Review}
