7c647f13117dc14b84f96bfd0639a75f696f1243
Jake Swackhammer <j.100101@gmail.com>
2021-11-01 20:56:01 -0400

    fixes 53

--- routes/api/placesRoutes.js
+++ routes/api/placesRoutes.js
@@ -1,7 +1,22 @@
 const router = require('express').Router();
 const { Places } = require('../../models');
+const { Venue } = require('../../models');
 const withAuth = require('../../utils/auth');
 
+//route used by search bar on the homepage
+router.get('/:city', async (req, res) => {
+  let foundCity = await Venue.findOne({
+    where:{
+      city: req.params.city
+    }
+  })
+
+  if(!foundCity){
+    res.status(404).json({message: `Could not find venue with that city.`})
+    return
+  }
+  res.json(foundCity.toJSON())
+})
 
 router.post('/', withAuth, async (req, res) => {
   try {
