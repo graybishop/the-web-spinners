20630b1161cc19ed1b77879fb9210fad89a17742
Jake Swackhammer <j.100101@gmail.com>
2021-11-02 11:32:28 -0400

    set-up card tailwinds styling

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -39,11 +39,38 @@ router.get('/home2', async (req, res) => {
   })
 })
 
+router.get('/home3', async (req, res) => {
+  let firstSetData = await Venue.findAll({limit:6})
+  
+  const maxTextSize = 150 //higher numbers mean more letters on the homepage cards descriptions
+  let firstSet = firstSetData.map(element => {
+    let data = element.toJSON()
+    if(data.description.length > maxTextSize){
+      data.description = data.description.substr(0, maxTextSize) + '...'
+    }
+    return data
+  })
+
+  res.render('hometw', {
+    title: 'Unearthly Venues',
+    venues: firstSet,
+    layout: 'maintw'
+  })
+})
+
 router.get('/login', async (req, res) => {
   res.render('login', {
     title: 'Login',
     layout: 'bs-main'
   })
 })
+
+router.get('/form', (req, res) => {
+  res.render("form");
+});
+
+router.get('/saved', (req, res) => {
+  res.render("saved");
+});
 
 module.exports = router
