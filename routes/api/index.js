c2c7376b1d5588f9b6cb7d1a1f8ab6d3fb341a9f
Jake Swackhammer <j.100101@gmail.com>
2021-11-02 22:38:57 -0400

    fixes #97

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 
-const placesRoutes = require('./placesRoutes.js');
+const venuesRoutes = require('./venuesRoutes.js');
 const userRoutes = require('./userRoutes.js');
 const eventRoutes = require('./eventsRoutes.js')
 
 router.use('/users', userRoutes);
-router.use('/venues', placesRoutes)
+router.use('/venues', venuesRoutes)
 router.use('/events', eventRoutes)
 module.exports = router;
 
