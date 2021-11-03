a541473dcce2852c4c3eb138f38650178bc2baf0
Jake Swackhammer <j.100101@gmail.com>
2021-11-02 22:51:17 -0400

    fixes #95

--- routes/api/eventsRoutes.js
+++ routes/api/eventsRoutes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { where } = require('sequelize/types');
 const { Event } = require('../../models');
 
 router.post('/', async (req, res) => {
@@ -34,5 +35,43 @@ router.delete('/:id', async (req, res) => {
   res.status(200).json(eventData);
 
 });
+
+//get route for event by id. Not sure if it will be used
+router.get('/:id', async (req, res) =>{
+  if (!req.session.loggedIn) {
+    res.redirect('/login');
+    return;
+  }
+
+  const eventData = await Event.findByPk(req.params.id)
+
+  if (!eventData){
+    res.status(404).json({message: 'Cannot find Event'})
+    return
+  }
+
+  res.json(eventData.toJSON())
+})
+
+//get route for event by userId
+router.get('/by-user/:userId', async (req, res) =>{
+  if (!req.session.loggedIn) {
+    res.redirect('/login');
+    return;
+  }
+
+  const eventsData = await Event.findAll({
+    where: {
+      userId: req.params.userId
+    }
+  })
+
+  if (!eventsData){
+    res.status(404).json({message: 'Cannot find events or user'})
+    return
+  }
+
+  res.json(eventsData.toJSON())
+})
 
 module.exports = router;
