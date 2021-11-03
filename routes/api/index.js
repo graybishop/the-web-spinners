32f81ea9f26479d30384b81852746aae8bf9af6d
Jake Swackhammer <j.100101@gmail.com>
2021-11-02 22:35:11 -0400

    added eventRoutes to api index

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const placesRoutes = require('./placesRoutes.js');
 const userRoutes = require('./userRoutes.js');
+const eventRoutes = require('./eventsRoutes.js')
 
 router.use('/users', userRoutes);
 router.use('/venues', placesRoutes)
+router.use('/events', eventRoutes)
 module.exports = router;
 
