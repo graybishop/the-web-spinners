e9fd95df1880bbf2b07dbcbb7b7b9195808b004c
Jake Swackhammer <j.100101@gmail.com>
2021-11-03 17:46:26 -0400

    fixes #65

--- public/js/home.js
+++ public/js/home.js
@@ -7,30 +7,30 @@ const searchForCity = async (event) => {
     return;
   }
 
-  let lowercaseCity = city.toLowerCase() 
+  let lowercaseCity = city.toLowerCase();
 
-  let capitalizedCity = lowercaseCity.charAt(0).toUpperCase() + lowercaseCity.slice(1)
+  let capitalizedCity = lowercaseCity.charAt(0).toUpperCase() + lowercaseCity.slice(1);
 
   const response = await fetch(`/api/venues/${capitalizedCity}`);
 
   if (response.ok) {
-    let responseVenue = await response.json()
-    console.log(responseVenue)
-    renderVenueCard(responseVenue)
+    let responseVenue = await response.json();
+    console.log(responseVenue);
+    renderVenueCard(responseVenue);
   } else {
     alert('Failed to find city.');
-    return
+    return;
   }
 
 };
 
 const renderVenueCard = (venue) => {
-  let resultsContainer = document.querySelector('#resultsContainer')
+  let resultsContainer = document.querySelector('#resultsContainer');
 
- 
-  
+
+
   let htmlString2 =
-  `<div class="flex-none px-2 md:w-3/12  sm:w-full">
+    `<div class="flex-none px-2 md:w-3/12  sm:w-full">
   <div class="bg-white-400 py-2 flex items-center justify-center w-full h-full">
       <div class="bg-white rounded-lg shadow-2xl w-full h-full">
           <header class="bg-gray-100 rounded-t-lg p-2 text-xl font-extrabold">
@@ -43,41 +43,39 @@ const renderVenueCard = (venue) => {
       </div>
   </div>
 </div>
-  `
-  resultsContainer.insertAdjacentHTML('beforeend',htmlString2)
-}
+  `;
+  resultsContainer.insertAdjacentHTML('beforeend', htmlString2);
+};
 
-const toggleStar = async (event) => {
-  let target = event.target
-  let parent = target.parentElement
+const addSavedVenueFromStar = async (event) => {
+  let target = event.target;
+  let parent = target.parentElement;
 
-  if (parent.tagName !== 'SPAN'){
-    return
+  if (parent.tagName !== 'SPAN') {
+    return;
   }
 
- 
-  //for toggling star visibility
-  let spans = parent.parentElement.children
-  for (const element of spans) {
-    element.classList.toggle('hidden')
+  const venue = target.dataset.venueId;
+  console.log(venue);
+  if (venue) {
+    const response = await fetch('/api/users/saved-venues', {
+      method: 'POST',
+      body: JSON.stringify({ venue }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      //for toggling star visibility
+      let spans = parent.parentElement.children;
+      for (const element of spans) {
+        element.classList.toggle('hidden');
+      }
+    } else {
+      alert(response.statusText);
+    }
   }
 
-  // const venue = Math.floor(Math.random() * 2000 + 2000);
-  // console.log(venue);
-  // if (venue) {
-  //   const response = await fetch('/api/users/saved-venues', {
-  //     method: 'POST',
-  //     body: JSON.stringify({ venue }),
-  //     headers: { 'Content-Type': 'application/json' },
-  //   });
-
-  //   if (response.ok) {
-  //     document.location.replace('/dashboard');
-  //   } else {
-  //     alert(response.statusText);
-  //   }
-  // }
-}
+};
 
 
 window.addEventListener('DOMContentLoaded', () => {
@@ -85,11 +83,14 @@ window.addEventListener('DOMContentLoaded', () => {
 
   //Home hero carousel
   // eslint-disable-next-line no-undef
-  new Splide( '#image-slider', {
-		cover      : true,
-		heightRatio: 0.3,
-  } ).mount();
+  new Splide('#image-slider', {
+    cover: true,
+    heightRatio: 0.3,
+  }).mount();
 
-  document.querySelector('.addSavedVenue').addEventListener('click', toggleStar)
-  document.querySelector('.removeSavedVenue').addEventListener('click', toggleStar)
+  let allSaveStars = document.querySelectorAll('.addSavedVenue')
+  for (const element of allSaveStars) {
+    element.addEventListener('click', addSavedVenueFromStar);
+  }
+
 });
