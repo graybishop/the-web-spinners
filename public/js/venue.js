2b850cf388888be91628478ad3e301f316e68a44
Jake Swackhammer <j.100101@gmail.com>
2021-11-03 13:01:56 -0400

    fixes #109

--- public/js/venue.js
+++ public/js/venue.js
@@ -3,10 +3,30 @@ const toggleEventModal = () => {
   eventModal.classList.toggle('hidden')
 }
 
-const submitNewEvent = (event) => {
+const submitNewEvent = async (event) => {
   event.preventDefault()
   console.log(event.target)
+
+  const name = document.querySelector('#eventName').value.trim();
+  const description = document.querySelector('#eventDescription').value.trim();
+  const date = document.querySelector('#eventDate').value
+  const venueId = event.target.dataset.venueId;
+
+  if (name && description && date && venueId){
+    const response = await fetch('/api/events', {
+      method: 'POST',
+      body: JSON.stringify({ venueId, date, name, description }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+  
+    if (response.ok) {
+      location.reload();
+    } else {
+      alert(response.json());
+    }
+  }
 }
+
 
 document.addEventListener('DOMContentLoaded', () => {
   document.querySelector('#bookEventButton').addEventListener('click', toggleEventModal);
