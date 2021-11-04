5731a650fb6ebe929902d17124695c0f9f276cff
Mohamed Lazrek <mohamed.lazrekb@gmail.com>
2021-11-04 14:53:35 -0400

    fixes #137

--- public/js/locationgit.js
+++ public/js/locationgit.js
@@ -10,7 +10,7 @@ navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
 
 const successfulLookup = position => {
     const { latitude, longitude } = position.coords;
-    fetch(`https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=6457baeadd874c80bd7a70f9ff672e82`)
+    fetch(`https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=6457baeadd874c80bd7a70f9ff672e82&no_anotations=1`)
         .then(response => response.json())
         .then(console.log)
 };
