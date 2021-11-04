07b61ecf50cfddfee3ff43a0c64f91e34cd01fab
Jake Swackhammer <j.100101@gmail.com>
2021-11-04 17:42:29 -0400

    turned posistion request into a promise

--- public/js/home.js
+++ public/js/home.js
@@ -99,6 +99,31 @@ const linkToDashboardVenue = (event) => {
   document.location.href = `/dashboard#sectionFor${venue}`;
 };
 
+// User Location Section
+const getPosition = () => new Promise((resolve, reject) => navigator.geolocation.getCurrentPosition(resolve, reject));
+
+const getUserCityState = async () => {
+    let position;
+    try {
+        position = await getPosition();
+    } catch (error) {
+        console.log(error);
+        return { error };
+    }
+
+    const { latitude, longitude } = position.coords;
+    let geoData;
+    try {
+        let geoResponse = await fetch(`https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=6457baeadd874c80bd7a70f9ff672e82&no_anotations=1`);
+        geoData = await geoResponse.json();
+    } catch (error) {
+        console.log(error);
+        return { error };
+    }
+    let { city, state } = geoData.results[0].components;
+    return { city, state };
+};
+// END User Location Section
 
 window.addEventListener('DOMContentLoaded', () => {
 
