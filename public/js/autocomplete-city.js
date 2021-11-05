18409793b85f6ebf89dcf21fbd041842be362410
Sheralyn Files <gired0329@yahoo.com>
2021-11-05 10:37:43 -0400

    intro to homepage

--- public/js/autocomplete-city.js
+++ public/js/autocomplete-city.js
@@ -27,3 +27,8 @@ const cityName = ["Aberdeen", "Abilene", "Akron", "Albany", "Albuquerque", "Alex
 "Temecula", "Tempe", "Thornton", "Thousand Oaks", "Toledo", "Topeka", "Torrance", "Trenton", "Tucson", "Tulsa", "Tuscaloosa", "Tyler", "Utica", "Vallejo", "Vancouver", "Vero Beach", 
 "Victorville", "Virginia Beach", "Visalia", "Waco", "Warren", "Washington", "Waterbury", "Waterloo", "West Covina", "West Valley City", "Westminster", "Wichita", "Wilmington", "Winston", 
 "Winter Haven", "Worcester", "Yakima", "Yonkers", "York", "Youngstown"];
+
+const stateNames = ["Alaska","Alabama","Arkansas","Arizona","California","Colorado","Connecticut","District of Columbia","Delaware","Florida","Georgia","Hawaii","Iowa",
+"Idaho","Illinois","Indiana","Kansas","Kentucky","Louisiana","Massachusetts","Maryland","Maine","Michigan","Minnesota","Missouri","Mississippi","Montana","North Carolina","North Dakota","Nebraska",
+"New Hampshire","New Jersey","New Mexico","Nevada","New York","Ohio","Oklahoma","Oregon","Pennsylvania","Rhode Island","South Carolina","South Dakota","Tennessee","Texas","Utah",
+"Virginia","Vermont","Washington","Wisconsin","West Virginia","Wyoming"];
